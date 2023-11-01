71ef0e7c6fb3b8e2b9c31bb1c9fd0495ed2ede9d
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2023-11-01 13:51:44 +0100

    refactor: color table (#150)

--- src/components/Colors/ColorTable/color-table.tsx
+++ src/components/Colors/ColorTable/color-table.tsx
@@ -1,67 +1,153 @@
-import { DBCard } from "@db-ui/react-components";
 import { ALL_VARIABLES } from "../../../utils/data.ts";
 import "./index.scss";
 import { useThemeBuilderStore } from "../../../store";
+import { useTranslation } from "react-i18next";
+import {
+  DBButton,
+  DBDivider,
+  DBIcon,
+  DBPopover,
+} from "@db-ui/react-components";
+import { prefix } from "../../../utils/outputs.ts";
+import { Fragment } from "react";
+
+const hasInvertedTextColor = (key: string, darkMode: boolean): boolean => {
+  if (darkMode) {
+    return (
+      key.includes("origin") || key.includes("text") || key === "on-bg-enabled"
+    );
+  }
+  return (
+    key.includes("origin") ||
+    key.includes("text") ||
+    key.includes("element") ||
+    key.includes("border") ||
+    key.includes("on-bg")
+  );
+};
 
 const ColorTable = () => {
-  const { colors } = useThemeBuilderStore((state) => state);
-  return (
-    <DBCard className="color-table-container" spacing="small">
-      {colors && colors.length > 0 ? (
-        <div className="table-scroll-container">
-          <table className="color-table">
-            <thead>
-              <tr>
-                <th className="variable-header">Variable</th>
-                {colors.map((color) => (
-                  <th key={`${color.name}-header`}>{color.name}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {ALL_VARIABLES.filter((varKey) => varKey.endsWith("enabled")).map(
-                (varKey) => (
-                  <tr key={`${varKey}-row`}>
-                    <td>{varKey}</td>
-                    {colors.map((color: any) => {
-                      const style: any = { "--color": color[varKey] };
-                      let title = color[varKey];
-                      const hoverColor =
-                        color[varKey.replace("enabled", "hover")];
-                      const pressedColor =
-                        color[varKey.replace("enabled", "pressed")];
-                      style["--color-hover"] = hoverColor;
-                      style["--color-pressed"] = pressedColor;
+  const { t } = useTranslation();
+  const { colors, darkMode } = useThemeBuilderStore((state) => state);
 
-                    title += `, ${hoverColor}, ${pressedColor}`;
-                    return (
-                      <td
-                        data-text-align="center"
-                        data-size="small"
-                        key={`${varKey}-${color.name}-cell`}
-                      >
-                        {color[varKey] ? (
-                          <div
-                            className="color-box"
-                            style={style}
-                            title={title}
-                          />
-                        ) : (
-                          "---"
-                        )}
-                      </td>
-                    );
-                  })}
-                </tr>
-              ),
-              )}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <span>No Colors found</span>
-      )}
-    </DBCard>
+  if (!colors || colors.length === 0) {
+    return null;
+  }
+
+  const enabledVariables = ALL_VARIABLES.filter((varKey) =>
+    varKey.endsWith("enabled"),
+  );
+
+  const copy = (notification: string, text: string) => {
+    navigator.clipboard.writeText(text || "");
+    useThemeBuilderStore.setState({
+      notification: `${notification} ${t("copied")}`,
+    });
+  };
+
+  return (
+    <div className="flex gap-fix-2xs">
+      <div className="grid grid-rows grid-flow-col gap-fix-2xs items-center">
+        <span></span>
+        {enabledVariables.map((varKey) => (
+          <span className="font-bold whitespace-nowrap text-end pr-fix-xs md:pr-fix-lg">
+            {t(varKey)}
+          </span>
+        ))}
+      </div>
+
+      <div
+        className="grid grid-rows grid-flow-col gap-fix-2xs text-center 
+      overflow-x-auto md:overflow-x-hidden w-full h-full overflow-y-hidden"
+      >
+        {colors.map((color: any, colorIndex: number) => (
+          <Fragment key={`${color.name}-header`}>
+            <span className="font-bold pb-fix-sm">{color.name}</span>
+
+            {enabledVariables.map((varKey) => {
+              const style: any = {
+                "--color": color[varKey],
+              };
+              const name = color.name;
+              const enabled = varKey;
+              const hover = varKey.replace("enabled", "hover");
+              const pressed = varKey.replace("enabled", "pressed");
+              const states = [enabled, hover, pressed];
+              const hoverColor = color[hover];
+              const pressedColor = color[pressed];
+              style["--color-hover"] = hoverColor;
+              style["--color-pressed"] = pressedColor;
+
+              return (
+                <div
+                  key={`${varKey}-${color.name}-cell`}
+                  className="color-box"
+                  style={style}
+                >
+                  <DBIcon
+                    icon="info"
+                    className={`my-auto ml-auto mr-fix-sm ${
+                      hasInvertedTextColor(varKey, darkMode) && "inverted"
+                    }`}
+                  >
+                    Info
+                    <DBPopover
+                      className="db-ui-functional"
+                      spacing="small"
+                      delay="slow"
+                      animation="disabled"
+                      gap
+                      placement={colorIndex < 3 ? "right" : "left"}
+                    >
+                      {states.map((state, index) => (
+                        <div
+                          key={`${name}-${state}`}
+                          className="flex flex-col w-[368px]"
+                        >
+                          <div className="flex justify-between items-center w-full">
+                            <span className="font-bold col-span-4 whitespace-nowrap">{`--${prefix}-${name}-${state}`}</span>
+                            <DBButton
+                              icon="copy"
+                              variant="text"
+                              noText
+                              onClick={() => {
+                                copy("Token", `--${prefix}-${name}-${state}`);
+                              }}
+                            >
+                              Copy
+                            </DBButton>
+                          </div>
+                          <div className="flex justify-between items-center w-full">
+                            <div className="flex gap-fix-sm items-center">
+                              <div
+                                className="color-preview"
+                                style={{ backgroundColor: color[state] }}
+                              />
+                              <span>HEX: {color[state]}</span>
+                            </div>
+                            <DBButton
+                              icon="copy"
+                              variant="text"
+                              noText
+                              onClick={() => {
+                                copy(t("color"), color[state]);
+                              }}
+                            >
+                              Copy
+                            </DBButton>
+                          </div>
+                          {index !== states.length - 1 && <DBDivider />}
+                        </div>
+                      ))}
+                    </DBPopover>
+                  </DBIcon>
+                </div>
+              );
+            })}
+          </Fragment>
+        ))}
+      </div>
+    </div>
   );
 };
 
