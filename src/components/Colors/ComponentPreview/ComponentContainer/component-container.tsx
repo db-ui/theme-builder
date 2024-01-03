528e929382ebd2e1448e75892d26c0411dde9285
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-01-03 15:05:43 +0100

    fix: issue with background color for card

--- src/components/Colors/ComponentPreview/ComponentContainer/component-container.tsx
+++ src/components/Colors/ComponentPreview/ComponentContainer/component-container.tsx
@@ -9,7 +9,6 @@ import {
 } from "@db-ui/react-components";
 import "./index.scss";
 import { DefaultVariantType } from "@db-ui/react-components/dist/shared/model";
-import { useThemeBuilderStore } from "../../../../store";
 
 const variants: DefaultVariantType[] = [
   "adaptive",
@@ -18,71 +17,61 @@ const variants: DefaultVariantType[] = [
   "warning",
   "successful",
 ];
-const ComponentContainer = () => {
-  const { darkMode, defaultColors } = useThemeBuilderStore((state) => state);
-  return (
-    <DBCard className="w-full flex flex-col" spacing="medium">
-      <div
-        style={{
-          backgroundColor: darkMode
-            ? defaultColors.onBgBase
-            : defaultColors.bgBase,
-        }}
-        className="flex flex-col gap-res-sm h-full"
-      >
+const ComponentContainer = () => (
+  <DBCard className="w-full flex flex-col" spacing="medium">
+    <div className="flex flex-col gap-res-sm h-full">
+      <div className="component-container">
+        <DBButton>Outlined Button</DBButton>
+        <DBButton variant="solid">Filled Button</DBButton>
+        <DBButton variant="text">Ghost Button</DBButton>
+        <DBButton variant="primary">Brand Button</DBButton>
+      </div>
+
+      <div className="component-container">
+        {variants.map((variant) => (
+          <DBInput
+            labelVariant="floating"
+            key={`input-${variant}`}
+            variant={variant}
+            label={variant}
+            placeholder={variant}
+            message={`${variant} Message`}
+          />
+        ))}
+      </div>
+      <div className="component-container"></div>
+      <div className="flex flex-col gap-fix-md">
         <div className="component-container">
-          <DBButton>Outlined Button</DBButton>
-          <DBButton variant="solid">Filled Button</DBButton>
-          <DBButton variant="text">Ghost Button</DBButton>
-          <DBButton variant="primary">Brand Button</DBButton>
+          {variants.map((variant) => (
+            <DBTag key={`tag-${variant}`} variant={variant}>
+              Tag {variant}
+            </DBTag>
+          ))}
         </div>
 
         <div className="component-container">
           {variants.map((variant) => (
-            <DBInput
-              labelVariant="floating"
-              key={`input-${variant}`}
+            <DBTag
+              key={`tag-strong-${variant}`}
               variant={variant}
-              label={variant}
-              placeholder={variant}
-              message={`${variant} Message`}
-            />
+              emphasis="strong"
+            >
+              Tag {variant}
+            </DBTag>
           ))}
         </div>
-        <div className="component-container"></div>
-        <div className="flex flex-col gap-fix-md">
-          <div className="component-container">
-            {variants.map((variant) => (
-              <DBTag key={`tag-${variant}`} variant={variant}>
-                Tag {variant}
-              </DBTag>
-            ))}
-          </div>
-
-          <div className="component-container">
-            {variants.map((variant) => (
-              <DBTag
-                key={`tag-strong-${variant}`}
-                variant={variant}
-                emphasis="strong"
-              >
-                Tag {variant}
-              </DBTag>
-            ))}
-          </div>
-        </div>
-
-        <div className="component-container">
-          <DBLink href="#">Text Link</DBLink>
-          <DBLink href="#" variant="primary">
-            Brand Text Link
-          </DBLink>
-          <DBCheckbox name="checkbox">Checkbox</DBCheckbox>
-          <DBRadio name="radio">Radio</DBRadio>
-        </div>
       </div>
-    </DBCard>
-  );
-};
+
+      <div className="component-container">
+        <DBLink href="#">Text Link</DBLink>
+        <DBLink href="#" variant="primary">
+          Brand Text Link
+        </DBLink>
+        <DBCheckbox name="checkbox">Checkbox</DBCheckbox>
+        <DBRadio name="radio">Radio</DBRadio>
+      </div>
+    </div>
+  </DBCard>
+);
 
 export default ComponentContainer;
