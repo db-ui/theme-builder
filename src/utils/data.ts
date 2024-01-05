3c2f326a11a7ff4aecf932dbb266f68161a21dc1
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-01-05 11:26:03 +0100

    chore: change default luminance

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -154,7 +154,7 @@ export type DefaultThemeType = {
 };
 
 export const defaultLuminances: number[] = [
-  0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 100,
+  0, 10, 20, 30, 40, 55, 65, 73, 80, 90, 95, 98, 100,
 ];
 
 export type HeisslufType = {
