b33f1b716e650700e449cb2cc7378004d2747406
Sina Han <sina.han@deutschebahn.com>
2024-02-20 12:33:41 +0100

    fix: extra brand colors included

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -35,7 +35,7 @@ export const getPalette = (allColors: object, luminanceSteps: number[]): any =>
       const color = value[1];
       const hslColors: HeisslufType[] = getHeissluftColors(
         color,
-        luminanceSteps,
+        luminanceSteps
       );
 
       return {
@@ -44,45 +44,86 @@ export const getPalette = (allColors: object, luminanceSteps: number[]): any =>
     })
     .reduce(
       (previousValue, currentValue) => ({ ...previousValue, ...currentValue }),
-      {},
+      {}
     );
-    const mergeColorsWithSpeakingNames = (
-      speakingNames: Record<string, string>,
-      colors: Record<string, string>
-    ): Record<string, string> => {
-      const result: Record<string, string> = {};
+const mergeColorsWithSpeakingNames = (
+  speakingNames: Record<string, string>,
+  colors: Record<string, string>
+): Record<string, string> => {
+  const result: Record<string, string> = {};
 
-      Object.entries(speakingNames).forEach(([speakingName, value]) => {
-        const transparencyMatch = value.match(/transparency (\d+%)/);
-        const transparency = transparencyMatch ? transparencyMatch[1] : '0%'; 
+  Object.entries(speakingNames).forEach(([speakingName, value]) => {
+    let transparency = "0%";
+    let colorHex = "";
 
-        const variableMatch = value.match(/var\((.*?)\)/);
-        const variableName = variableMatch ? variableMatch[1] : '';
-        const colorHex = variableName && colors.hasOwnProperty(variableName) ? colors[variableName] : '';
-        
-        result[speakingName] = `${transparency}, ${colorHex}`;
-      });
-      return result;
-    };
+    const transparencyMatch = value.match(/transparency (\d+%)/);
+    if (transparencyMatch) {
+      transparency = transparencyMatch[1];
+    }
+
+    if (speakingName.startsWith("--")) {
+      if (speakingName.includes("on-enabled")) {
+        speakingName = `On/Brand/01-Enabled`;
+      } else if (speakingName.includes("origin-enabled")) {
+        speakingName = `Brand/Origin/01-Enabled`;
+      } else if (speakingName.includes("origin-hover")) {
+        speakingName = `Brand/Origin/02-Hover`;
+      } else if (speakingName.includes("origin-pressed")) {
+        speakingName = `Brand/Origin/03-Pressed`;
+      }
+      colorHex = value;
+    } else {
+      const variableMatch = value.match(/var\((.*?)\)/);
+      const variableName = variableMatch ? variableMatch[1] : "";
+      if (variableName && colors.hasOwnProperty(variableName)) {
+        colorHex = colors[variableName];
+      }
+    }
+    result[speakingName] = `${transparency}, ${colorHex}`;
+  });
+
+  return result;
+};
 
 export const downloadTheme = async (
   speakingNames: SpeakingName[],
   luminanceSteps: number[],
   defaultTheme: DefaultThemeType,
   colorMapping: DefaultColorMappingType,
-  customColorMapping?: CustomColorMappingType,
+  customColorMapping?: CustomColorMappingType
 ) => {
   const theme: DefaultThemeType = { ...defaultTheme, colors: colorMapping };
 
   const allColors = { ...colorMapping, ...customColorMapping };
   const colors = getPaletteOutput(getPalette(allColors, luminanceSteps));
 
-  const lightSpeakingNames = getSpeakingNamesForJSON(speakingNames, allColors, true, luminanceSteps)
-  const darkSpeakingNames = getSpeakingNamesForJSON(speakingNames, allColors, false, luminanceSteps)
+  const lightSpeakingNames = getSpeakingNamesForJSON(
+    speakingNames,
+    allColors,
+    true,
+    luminanceSteps
+  );
+  const darkSpeakingNames = getSpeakingNamesForJSON(
+    speakingNames,
+    allColors,
+    false,
+    luminanceSteps
+  );
 
-  const lightSpeakingNamesWithHex = mergeColorsWithSpeakingNames(lightSpeakingNames, colors);
-  const darkSpeakingNamesWithHex = mergeColorsWithSpeakingNames(darkSpeakingNames, colors);
-  console.log("result light:" , lightSpeakingNamesWithHex, "result dark:" , darkSpeakingNamesWithHex);
+  const lightSpeakingNamesWithHex = mergeColorsWithSpeakingNames(
+    lightSpeakingNames,
+    colors
+  );
+  const darkSpeakingNamesWithHex = mergeColorsWithSpeakingNames(
+    darkSpeakingNames,
+    colors
+  );
+  console.log(
+    "result light:",
+    lightSpeakingNamesWithHex,
+    "result dark:",
+    darkSpeakingNamesWithHex
+  );
 
   const fileName = `default-theme`;
   const themeJsonString = JSON.stringify(theme);
@@ -96,23 +137,18 @@ export const downloadTheme = async (
   zip.file(`${fileName}.json`, themeJsonString);
   zip.file(`${fileName}-colors.json`, colorsJsonString);
   zip.file(`${fileName}-theme.css`, themeProperties);
-  zip.file(
-    `${fileName}-palette.css`,
-    getCssPropertyAsString(
-      colors,
-    ),
-  );
+  zip.file(`${fileName}-palette.css`, getCssPropertyAsString(colors));
   zip.file(
     `${fileName}-speaking-names-light.css`,
     getCssPropertyAsString(
-      getSpeakingNames(speakingNames, allColors, false, luminanceSteps),
-    ),
+      getSpeakingNames(speakingNames, allColors, false, luminanceSteps)
+    )
   );
   zip.file(
     `${fileName}-speaking-names-dark.css`,
     getCssPropertyAsString(
-      getSpeakingNames(speakingNames, allColors, true, luminanceSteps),
-    ),
+      getSpeakingNames(speakingNames, allColors, true, luminanceSteps)
+    )
   );
   const zipFile = await zip.generateAsync({ type: "blob" });
   download(`${fileName}.zip`, zipFile);
