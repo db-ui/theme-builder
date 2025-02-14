b0b57fcafd4b956e3926d540e6db40520115a483
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-02-14 13:02:23 +0100

    fix: issue with build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as url from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,12 +7,5 @@ export default defineConfig({
   plugins: [react()],
   define: {
     global: {},
-  },
-  resolve: {
-    alias: {
-      "@db-ux": url.fileURLToPath(
-        new URL("./node_modules/@db-ux", import.meta.url),
-      ),
-    },
-  },
+  }
 });
